c0775b3b3f2dd091687987b1ed6ba130452f9beb
MaxHwoy <max.hwoy@gmail.com>
2020-07-03 21:54:20 -0700

    Added AddEndscriptCommand

---
+++ Endscript/Commands/AddEndScriptCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Endscript.Core;
+
+namespace Endscript.Commands
+{
+	public sealed class AddEndScriptCommand : BaseEndScriptCommand
+	{
+		private int _level;
+		private string _filename;
+		private string _manager;
+		private string _collection;
+
+		public override void Execute(CollectionMap map) => throw new NotImplementedException();
+		public override void Prepare(string[] splits) => throw new NotImplementedException();
+	}
+}
--- Endscript/Commands/UpdateEndScriptCommand.cs
+++ Endscript/Commands/UpdateEndScriptCommand.cs
@@ -1,13 +1,85 @@
 ﻿using System;
 using Endscript.Core;
-
-
+using Endscript.Exceptions;
 
 namespace Endscript.Commands
 {
 	class UpdateEndScriptCommand : BaseEndScriptCommand
 	{
-		public override void Execute(CollectionMap map) => throw new NotImplementedException();
-		public override void Prepare(string[] splits) => throw new NotImplementedException();
+		private int _level;
+		private string _filename;
+		private string _manager;
+		private string _collection;
+		private string _expand;
+		private string _subpart;
+		private string _property;
+		private string _value;
+
+		public override void Prepare(string[] splits)
+		{
+			this._level = splits.Length;
+
+			switch (this._level)
+			{
+				case 6:
+					this._filename = splits[1];
+					this._manager = splits[2];
+					this._collection = splits[3];
+					this._property = splits[4];
+					this._value = splits[5];
+					return;
+
+				case 7:
+					this._filename = splits[1];
+					this._manager = splits[2];
+					this._collection = splits[3];
+					this._expand = splits[4];
+					this._property = splits[5];
+					this._value = splits[6];
+					return;
+
+				case 8:
+					this._filename = splits[1];
+					this._manager = splits[2];
+					this._collection = splits[3];
+					this._expand = splits[4];
+					this._subpart = splits[5];
+					this._property = splits[6];
+					this._value = splits[7];
+					return;
+
+				default: // invalid
+					return;
+
+			}
+		}
+
+		public override void Execute(CollectionMap map)
+		{
+			switch (this._level)
+			{
+				case 6: this.UpdateCollection(map); return;
+				case 7: this.UpdateSTR_TPK(map); return;
+				case 8: this.UpdateSubPart(map); return;
+				default:
+					if (!ThrowError) return;
+					throw new InvalidArgsNumberException(this._level, 6, 7, 8);
+			}
+		}
+
+		private void UpdateCollection(CollectionMap map)
+		{
+
+		}
+
+		private void UpdateSTR_TPK(CollectionMap map)
+		{
+
+		}
+
+		private void UpdateSubPart(CollectionMap map)
+		{
+
+		}
 	}
 }
